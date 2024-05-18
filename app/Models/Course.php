e4660d0065a96fefa16291460320e6ac1cede2f5
NathanaelGT <gordontanu2004@gmail.com>
2024-05-18 21:32:46 +0700

    chore: Relationship

--- app/Models/Course.php
+++ app/Models/Course.php
@@ -7,6 +7,7 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Course extends Model
 {
@@ -39,5 +40,10 @@ class Course extends Model
     public function professors(): BelongsToMany
     {
         return $this->belongsToMany(Professor::class);
+    }
+
+    public function subjects(): HasMany
+    {
+        return $this->hasMany(Subject::class);
     }
 }
