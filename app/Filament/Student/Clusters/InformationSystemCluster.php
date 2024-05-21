bc736cbac3e8dda4fe183e5b5779f40bb580524d
NathanaelGT <gordontanu2004@gmail.com>
2024-05-21 17:58:13 +0700

    chore: Membuat cluster Sistem Informasi

---
+++ app/Filament/Student/Clusters/InformationSystemCluster.php
@@ -0,0 +1,14 @@
+<?php
+
+namespace App\Filament\Student\Clusters;
+
+use Filament\Clusters\Cluster;
+
+class InformationSystemCluster extends Cluster
+{
+    protected static ?string $navigationIcon = 'heroicon-o-squares-2x2';
+
+    protected static ?string $title = 'Sistem Informasi';
+
+    protected static ?string $slug = 'sistem-informasi';
+}
