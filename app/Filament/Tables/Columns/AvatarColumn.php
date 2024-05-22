86d0c5cf33011c262b13eadf0c0b1efbdcabdae3
NathanaelGT <gordontanu2004@gmail.com>
2024-05-22 23:14:47 +0700

    chore: Membuat model AvatarColumn bisa dikustomisasi

--- app/Filament/Tables/Columns/AvatarColumn.php
+++ app/Filament/Tables/Columns/AvatarColumn.php
@@ -4,23 +4,41 @@ namespace App\Filament\Tables\Columns;
 
 use App\Models\Contracts\HasAccountContract;
 use App\Models\User;
+use Closure;
 use Filament\AvatarProviders\UiAvatarsProvider;
 use Filament\Tables\Columns\ImageColumn;
+use Illuminate\Database\Eloquent\Model;
 
 class AvatarColumn extends ImageColumn
 {
+    protected ?Closure $resolveAccountUsing = null;
+
     public static function make(string $name): static
     {
         return parent::make($name)
-            ->defaultImageUrl(function (User | HasAccountContract $record) {
+            ->defaultImageUrl(function (self $avatarColumn, $record) {
                 static $provider = new UiAvatarsProvider();
 
-                $account = $record instanceof HasAccountContract
-                    ? $record->account
-                    : $record;
-
-                return $provider->get($account);
+                return $provider->get($avatarColumn->resolveAccount($record));
             })
             ->circular();
+    }
+
+    public function resolveAccountUsing(Closure $resolveAccountUsing): static
+    {
+        $this->resolveAccountUsing = $resolveAccountUsing;
+
+        return $this;
+    }
+
+    protected function resolveAccount(Model $model): User | HasAccountContract
+    {
+        if ($this->resolveAccountUsing) {
+            return $this->evaluate($this->resolveAccountUsing);
+        }
+
+        return $model instanceof HasAccountContract
+            ? $model->account
+            : $model;
     }
 }
