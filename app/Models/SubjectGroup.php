2d6bb455878ad0e0765cba96a1c49ca640bc4f59
NathanaelGT <gordontanu2004@gmail.com>
2024-06-04 23:56:09 +0700

    chore: Melengkapi relation `SubjectGroup`

--- app/Models/SubjectGroup.php
+++ app/Models/SubjectGroup.php
@@ -5,6 +5,8 @@ namespace App\Models;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Relations\BelongsToMany;
+use Illuminate\Database\Eloquent\Relations\MorphMany;
 
 class SubjectGroup extends Model
 {
@@ -12,8 +14,23 @@ class SubjectGroup extends Model
 
     const UPDATED_AT = null;
 
+    protected $fillable = [
+        'name',
+        'subject_id',
+    ];
+
     public function subject(): BelongsTo
     {
         return $this->belongsTo(Subject::class);
+    }
+
+    public function members(): BelongsToMany
+    {
+        return $this->belongsToMany(Student::class, 'subject_group_members');
+    }
+
+    public function submissions(): MorphMany
+    {
+        return $this->morphMany(Submission::class, 'submissionable');
     }
 }
