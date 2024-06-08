d51411e700aac77b701cdb71f137e752c62a780f
NathanaelGT <gordontanu2004@gmail.com>
2024-06-08 18:47:56 +0700

    feat: Mengecek masa persiapan krs pada staff

--- app/Filament/Staff/Resources/SubjectResource/Pages/ListSubjects.php
+++ app/Filament/Staff/Resources/SubjectResource/Pages/ListSubjects.php
@@ -4,9 +4,11 @@ namespace App\Filament\Staff\Resources\SubjectResource\Pages;
 
 use App\Filament\Staff\Resources\SubjectResource;
 use App\Models\Semester;
+use App\Period\Period;
 use Filament\Actions;
 use Filament\Resources\Pages\ListRecords;
 use Illuminate\Database\Eloquent\Builder;
+use Illuminate\Support\Facades\Gate;
 
 class ListSubjects extends ListRecords
 {
@@ -15,13 +17,14 @@ class ListSubjects extends ListRecords
     protected function getHeaderActions(): array
     {
         return [
-            Actions\CreateAction::make(),
+            Actions\CreateAction::make()
+                ->authorize(Gate::check(Period::KRSPreparation)),
         ];
     }
 
     protected function getTableQuery(): ?Builder
     {
         return parent::getTableQuery()
-            ->where('semester_id', Semester::query()->current()->select('id'));
+            ->where('semester_id', Semester::current()->id);
     }
 }
