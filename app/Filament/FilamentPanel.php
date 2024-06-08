755f5fc5bce596f6d54419ab6300a941bd615057
NathanaelGT <gordontanu2004@gmail.com>
2024-06-08 19:38:13 +0700

    feat: Memperingati jika pengguna berganti/keluar halaman saat ada perubahan yang belum disimpan

--- app/Filament/FilamentPanel.php
+++ app/Filament/FilamentPanel.php
@@ -34,6 +34,7 @@ abstract class FilamentPanel
             ->discoverWidgets(in: app_path("Filament/$name/Widgets"), for: "App\\Filament\\$name\\Widgets")
             ->discoverClusters(in: app_path("Filament/$name/Clusters"), for: "App\\Filament\\$name\\Clusters")
             ->discoverResources(in: app_path("Filament/$name/Resources"), for: "App\\Filament\\$name\\Resources")
+            ->unsavedChangesAlerts()
             ->databaseTransactions()
             ->middleware([
                 EncryptCookies::class,
